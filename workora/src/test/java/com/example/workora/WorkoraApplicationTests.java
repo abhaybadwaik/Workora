c2329d9196609428d8e8d99e78150f0f69db30d4
abhaybadwaik <badwaikabhay@gmail.com>
2026-02-17 18:20:31 +0530

    Phase 1 - Spring Boot + PostgreSQL setup completed

---
+++ workora/src/test/java/com/example/workora/WorkoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
